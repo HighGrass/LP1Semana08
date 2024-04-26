46c43da9905aaa169024c11c5abd454eef71061a
matv <matv.oficial@gmail.com>
2024-04-26 21:39:07 +0100

    PlayerManager3 - Created

---
+++ PlayerManager3/Player.cs
@@ -0,0 +1,13 @@
+namespace PlayerManager3
+{
+    class Player
+    {
+        public Player(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+        public string Name { get; private set; }
+        public int Score { get; private set; }
+    }
+}
